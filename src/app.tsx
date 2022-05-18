57027b1318c884b42aaa91348dcd381dc3bc980d
Pavel Koryakin <unclekoin@gmail.com>
2022-05-18 17:24:50 +0300

    init: start project

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app.css';
+
+const App = () => {
+    return (
+        <div className="app">
+            <h1>React Chess</h1>
+        </div>
+    );
+};
+
+export default App;
